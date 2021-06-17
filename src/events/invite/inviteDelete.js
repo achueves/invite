9ea6fe080785e741e2cd99271094057f28c53dbe
vexdy <devindustries1337@gmail.com>
2021-06-17 20:31:12 +0300

    feat(event): add invite events

---
+++ src/events/invite/inviteDelete.js
@@ -0,0 +1,18 @@
+module.exports = async (client, invite) => {
+  // ? İlk başta bunun bilinçli bir silinme olup olmadığını kontrol etmemiz gerekiyor.
+  const fetchedLogs = await invite.guild.fetchAuditLogs({
+    limit: 1,
+  });
+
+  const deletionLog = fetchedLogs.entries.first();
+
+  // ? Eğer INVITE_DELETE işlemi loglarda yazıyorsa, birisi gelmemiştir.
+  if (deletionLog.action == "INVITE_DELETE") return;
+
+  const guildInvites = client.invites.get(invite.guild.id);
+  const findInvite = guildInvites.find((inv) => inv.code == invite.code);
+  const temporaryInvites = client.tempInvites();
+
+  temporaryInvites.unshift(findInvite);
+  client.invites.set(invite.guild.id, await invite.guild.fetchInvites());
+};

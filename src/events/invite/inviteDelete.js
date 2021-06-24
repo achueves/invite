46c75570d06003392a5edc4af330e48ec5684f14
wvwvwvw <44919558+vexdy@users.noreply.github.com>
2021-06-24 18:32:39 +0300

    fix: fetch invite after delete

--- src/events/invite/inviteDelete.js
+++ src/events/invite/inviteDelete.js
@@ -7,7 +7,7 @@ module.exports = async (client, invite) => {
   const deletionLog = fetchedLogs.entries.first();
 
   // ? Eğer INVITE_DELETE işlemi loglarda yazıyorsa, birisi gelmemiştir.
-  if (deletionLog.action == "INVITE_DELETE") return;
+  if (deletionLog.action == "INVITE_DELETE") return client.invites.set(invite.guild.id, await invite.guild.fetchInvites());
 
   const guildInvites = client.invites.get(invite.guild.id);
   const findInvite = guildInvites.find((inv) => inv.code == invite.code);
